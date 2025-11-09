5b60fb6993573db5ada2376706bab0a9b42a3512
BatogovRoman <romanbatogov@gmail.com>
2025-11-09 17:39:02 +0500

    Task2

---
+++ Tyuiu.BatogovRK.Sprint4.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BatogovRK.Sprint4.Task2.V23.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint4.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 9, 8, 7, 6, 5 };
+
+            int wait = 14;
+            int res = ds.Calculate(array);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint4.Task2.V23/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.BatogovRK.Sprint4.Task2.V23.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: Батогов Р.К. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнил: Батогов Роман Константинович | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine("*                                                                         *");
+
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[] Rnumbers = new int[15];
+        Random random = new Random();
+        for (int i = 0; i < Rnumbers.Length; i++)
+        {
+            Rnumbers[i] = random.Next(3, 9);
+        }
+
+        var str = string.Join(" ", Rnumbers);
+        Console.WriteLine(str);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        var result = ds.Calculate(Rnumbers);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
