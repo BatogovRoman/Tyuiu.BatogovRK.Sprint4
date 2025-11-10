036cbe9ee2a61c0fda2416d3c4a0725804ccbc3a
BatogovRoman <romanbatogov@gmail.com>
2025-11-10 14:07:50 +0500

    Task5

---
+++ Tyuiu.BatogovRK.Sprint4.Task5.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BatogovRK.Sprint4.Task5.V12.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint4.Task5.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] ms = { {-1, 5, 6 },
+                          {6, 5, -2} };
+            int res = ds.Calculate(ms);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint4.Task5.V12/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.BatogovRK.Sprint4.Task5.V12.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Батогов Р.К. | АСОиУб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема:  Двумерные массивы (Генератор случайных чисел)                    *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Выполнил: Батогов Роман Константинович | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* случайными значениями в диапазоне от -6 до 4. Найти количество          *");
+        Console.WriteLine("* отрицательных элементов                                                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Random random = new Random();
+        int[,] ms = new int[5, 5];
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                ms[i, j] = random.Next(-6, 4);
+
+                Console.Write("   " + ms[i, j]);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(ms);
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+    }
+}
