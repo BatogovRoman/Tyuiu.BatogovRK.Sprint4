b81cdaf1026272b24ad03b0faeee2e96908c8960
BatogovRoman <romanbatogov@gmail.com>
2025-11-10 14:36:54 +0500

    Task7

---
+++ Tyuiu.BatogovRK.Sprint4.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BatogovRK.Sprint4.Task7.V10.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint4.Task7.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            string str = "695847142536";
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 30;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
